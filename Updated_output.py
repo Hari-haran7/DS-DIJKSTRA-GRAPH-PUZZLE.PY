e4b9fbcd0a3a96ab0171690e229fcf13e04f0479
Hariharan <harihara4611@gmail.com>
2024-10-15 14:59:31 +0530

    Create Updated_output.py

---
+++ Updated_output.py
@@ -0,0 +1,34 @@
+Enter the 15-puzzle configuration (0 for blank space):
+Enter row 1 (4 numbers separated by spaces): 1 2 3 4 
+Enter row 2 (4 numbers separated by spaces): 5 6 7 8
+Enter row 3 (4 numbers separated by spaces): 9 10 11 12
+Enter row 4 (4 numbers separated by spaces): 0 13 14 15
+Solution found in 3 moves: RRR
+
+Initial State:
+ 1  2  3  4
+ 5  6  7  8
+ 9 10 11 12
+   13 14 15
+
+Move R:
+ 1  2  3  4
+ 5  6  7  8
+ 9 10 11 12
+13    14 15
+
+Move R:
+ 1  2  3  4
+ 5  6  7  8
+ 9 10 11 12
+13 14    15
+
+Move R:
+ 1  2  3  4
+ 5  6  7  8
+ 9 10 11 12
+13 14 15   
+
+=== Puzzle Solved ===
+
+=== Code Execution Successful ===
